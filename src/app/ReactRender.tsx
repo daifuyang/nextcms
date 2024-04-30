b5268314de3e1554dc93926ac789c5822edde146
daifuyang <1140444693@qq.com>
2024-04-30 17:41:49 +0800

    新增批量网络请求保存到state中

--- src/app/ReactRender.tsx
+++ src/app/ReactRender.tsx
@@ -5,35 +5,36 @@ export default async function ReactRender(props: any) {
     const { schema = {} } = props;
     const { dataSource = {} } = schema
     const apiList = dataSource?.list || []
-    // await fetchData(apiList)
+    const state = await fetchData(apiList)
+
+    schema.state = {...schema.state, ...state}
+    
     return <ReactRenderCore {...props} />
 }
 
-export function fetchData(apiList = []) {
-    const promises: any = []
-    apiList.forEach((item: any) => {
+export async function fetchData(apiList = []) {
+    const state: any = {}
+    for (let index = 0; index < apiList.length; index++) {
+        const item: any = apiList[index];
         if (item.type === 'editor') {
-            const { url, method, params, data } = item.options;
-            const promise = request.instance({
-                url,
-                method,
-                params,
-                data,
-            }).then((res) => {
-                // console.log('请求结果', res);
-                return res.data;
+            const { options } = item;
+            const response: any = await request.instance({
+                ...options,
+                url: options.uri,
+                method: options.method,
+                data: options.params,
+                headers: options.headers,
             })
-            promises.push(promise);
+
+            const res = response.data
+
+            if (response.status === 200) {
+                state[item.id] = {
+                    type: 'JSExpression',
+                    value: JSON.stringify(res.data)
+                }
+            }
         }
-    })
-
-    Promise.all(promises)
-        .then(results => {
-            console.log('所有请求完成', results);
-            // 处理每个请求的结果
-        })
-        .catch(error => {
-            console.error('至少有一个请求失败', error);
-        });
-
+    }
+    return state
 }
